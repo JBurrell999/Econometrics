08f22ed8a299fb348b5d1936b36e52afff071efb
James Burrell <134342563+JBurrell999@users.noreply.github.com>
2025-08-27 09:24:43 -0700

    in class quiz

--- compclass1.py
+++ compclass1.py
@@ -5,3 +5,16 @@ b = 4
 
 a + b == myName
 print(myName)
+
+# 1. Create the two lists
+list_1 = [2, 4, 6, 8, 10]     # even numbers from 2 to 10
+list_2 = [1, 3, 5, 7, 9]      # odd numbers from 1 to 9
+
+# 2. Use a for loop to create list_sum
+list_sum = []
+for i in range(len(list_1)):
+    list_sum.append(list_1[i] + list_2[i])
+
+print("list_1:", list_1)
+print("list_2:", list_2)
+print("list_sum:", list_sum)
