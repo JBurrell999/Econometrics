d8f221425e252822df5d120c22572a86415d81fa
James Burrell <134342563+JBurrell999@users.noreply.github.com>
2025-09-01 23:03:41 -0700

    hw 2

--- exercise.py
+++ exercise.py
@@ -4,6 +4,13 @@ type(test)
 
 from numpy import array
 
-x = [0.0, 1, 2, 4, 4]
-y = array(x)
-print(y)
+#x = [0.0, 1, 2, 4, 4]
+#y = array(x)
+#print(y)
+
+
+
+
+#Nested Arrays 
+y = array([0.0, 1, 2, 3, 4], [ 5, 6, 7, 8, 9])
+print (y)
